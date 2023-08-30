cd654b3403d15a94f97464575599747a7f7508a8
P SC AMRUTHA VARSHINI <120082740+AmruthaVarshinipsc@users.noreply.github.com>
2023-08-30 14:02:41 +0530

    Update static.cpp

--- static.cpp
+++ static.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class Human{
+public:
+	static int Human_count;
+	Human(){
+		Human_count++;
+	}
+	void Display(){
+		cout<<Human_count<<endl;
+	}
+	static void display(){
+		cout<<"Hi "<<Human_count<<endl;
+	}
+
+};
+int Human::Human_count = 0;
+int main()
+{
+	
+	
+	cout<<Human::Human_count<<endl;
+	Human Ashok;
+	Human::display();
+	Human Assu;
+	Ashok.Display();
+
+	
+}
