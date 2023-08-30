0d89d8d99a91f29e075f287f1e5ce72bd45050e2
P SC AMRUTHA VARSHINI <120082740+AmruthaVarshinipsc@users.noreply.github.com>
2023-08-30 14:30:46 +0530

    Update Inheritance.cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,1 +1,39 @@
+//Example 1
+#include<bits/stdc++.h>
+using namespace std;
 
+class Human{
+public:
+	string name;
+	int age;
+	void SetName(string myname){
+		name = myname;
+	}
+	void SetAge(int val){
+		age=val;
+	}
+	
+
+};
+class Student:public Human{
+public:
+	int id;
+	void SetId(int val){
+		id = val;
+	}
+	void display(){
+		cout<<name<<" "<<age<<" "<<id<<endl;
+	}
+};
+int main()
+{
+	
+	Student Ayan;
+	Ayan.id = 1234;
+	Ayan.name = "Ayan";
+	Ayan.SetName("Emlie");
+	Ayan.SetAge(23);
+	Ayan.display();
+
+	
+}
