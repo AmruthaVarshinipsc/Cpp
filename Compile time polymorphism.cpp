bd647afb465e221f66af6828adde501af9648343
P SC AMRUTHA VARSHINI <120082740+AmruthaVarshinipsc@users.noreply.github.com>
2023-09-13 14:52:11 +0530

    Update Compile time polymorphism.cpp

--- Compile time polymorphism.cpp
+++ Compile time polymorphism.cpp
@@ -54,3 +54,38 @@ int main(){
 
 
 
+
+//operator overloading method 2
+class Complex
+{
+private:
+	int x,y;
+public:
+	Complex(int i=0,int j=0){
+		x=i;
+		y=j;
+	}
+	void print(){
+		cout<<x<<"+"<<y<<"i"<<endl;
+	}
+	Complex add(Complex &c1){
+		Complex temp;
+		temp.x = c1.x+x;
+		temp.y = c1.y+y;
+		return temp;
+	}
+	
+};
+int main(){
+	Complex c1(3,4);
+	Complex c2(7,2);
+	c1.print();
+	c2.print();
+	Complex c3;
+	c3 = c2.add(c1);
+	c3.print();
+
+}
+
+
+
